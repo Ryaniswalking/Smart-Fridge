c3e96e61ba02de333e27613e4eb33a55354253b2
ryanwalker <ryanwalker55@gmail.com>
2025-02-04 00:02:01 -0800

    change to dash

---
+++ frontend/src/components/dash.js
@@ -0,0 +1,12 @@
+import React from "react";
+import WeatherComponent from "./WeatherComponent";
+
+const Dashboard = () => {
+    return (
+        <div className="dashboard">
+            <WeatherComponent />
+        </div>
+    )
+}
+
+export default Dashboard;
