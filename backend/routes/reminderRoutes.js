8e1f266e6be8db531de96d00fc672dcc70a47ab7
ryanwalker <ryanwalker55@gmail.com>
2025-03-20 22:12:01 -0700

    add saveReminder route

--- backend/routes/reminderRoutes.js
+++ backend/routes/reminderRoutes.js
@@ -1,15 +1,28 @@
 const express = require('express');
-const { getReminders } = require('../queries/queries')
+const { getReminders, saveReminder } = require('../queries/queries')
+const { parseReminder } = require('../models/reminder');
 
 const router = express.Router()
 
 router.get('/', async (req, res) => {
     try{
         const results = await getReminders();
-        res.json(results);
+        const reminders = parseReminder(results);
+        res.json(reminders)
+        // res.json(results);
     } catch (err) {
         console.error('Error fetching reminders:', err);
         res.status(500).status('Error fetching reminders');
+    }
+})
+
+router.post("/add-reminder", (req, res) => {
+    try{
+        console.log(req.body);
+        saveReminder(req.body);
+        res.status(200).send("successfully added");
+    }catch (err){
+        res.status(500).status("NAS QUEEN");
     }
 })
 
