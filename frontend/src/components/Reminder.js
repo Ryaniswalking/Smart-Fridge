bb0ce2b4ececc97d4446548e7941673b15c1add1
ryanwalker <ryanwalker55@gmail.com>
2025-04-06 23:33:22 -0700

    format with prettier

--- frontend/src/components/Reminder.js
+++ frontend/src/components/Reminder.js
@@ -1,33 +1,33 @@
-import React from 'react';
-import '../styles/reminders.css';
+import React from "react";
+import "../styles/reminders.css";
 
 function Reminder({ reminder, handleComplete }) {
-    return (
+  return (
+    <div>
+      <div>
+        <strong>{reminder.title}</strong>
+      </div>
+      <div>{reminder.description}</div>
+      <div>
+        <em>{new Date(reminder.reminderTime).toLocaleString()}</em>
+      </div>
+      <div>Created By: {reminder.createdBy}</div>
+      {reminder.isRecurring && (
         <div>
-            <div>
-                <strong>{reminder.title}</strong>
-            </div>
-            <div>{reminder.description}</div>
-            <div>
-                <em>{new Date(reminder.reminderTime).toLocaleString()}</em>
-            </div>
-            <div>Created By: {reminder.createdBy}</div>
-            {reminder.isRecurring && (
-                <div>
-                    Recurring: {reminder.frequency} until{' '}
-                    {new Date(reminder.recurrenceEnd).toLocaleDateString()}
-                </div>
-            )}
-            {reminder.status !== 'completed' && (
-                <button
-                    onClick={() => handleComplete(reminder)}
-                    className="complete-button"
-                >
-                    Complete
-                </button>
-            )}
+          Recurring: {reminder.frequency} until{" "}
+          {new Date(reminder.recurrenceEnd).toLocaleDateString()}
         </div>
-    );
+      )}
+      {reminder.status !== "completed" && (
+        <button
+          onClick={() => handleComplete(reminder)}
+          className="complete-button"
+        >
+          Complete
+        </button>
+      )}
+    </div>
+  );
 }
 
 export default Reminder;
