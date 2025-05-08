63004c74d68ed9bf5132816addc78a9277743636
ryanwalker <ryanwalker55@gmail.com>
2025-05-07 22:15:37 -0700

    update list path to get vert and hori photos

--- backend/routes/digitalFrameRoutes.js
+++ backend/routes/digitalFrameRoutes.js
@@ -1,21 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const path = require('path')
-const fs = require("fs");
-
-const photosPath = path.join(__dirname, '../photos');
+const path = require('path');
+const digitalFrameController = require('../controllers/DigitalFrameController');
 
 router.use("/", express.static(path.join(__dirname, '../photos')));
 
-router.get("/list", (req, res) => {
-  fs.readdir(photosPath, (err, files) => {
-    if(err) return res.status(500).json.json({error: "Failed to read photos directory."})
-    
-    const imageFiles = files.filter(file =>
-      /\.(jpg|jpeg|png|gif)$/i.test(file)
-    )
-    res.json(imageFiles)
-  })
-})
+router.get("/list", async (req, res) => {
+  try{
+    const {vertical, horizontal} = await digitalFrameController.compilePhotos();
+    res.json({vertical, horizontal})
+  } catch (err){
+    console.log("Error from router", err);
+    res.status(500).json({error: err})
+  }
+});
 
+  
 module.exports = router;
